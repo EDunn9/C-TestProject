b9b11a04cc5c8c8bea4d4b4cc882de094220ef04
Evan Dunn <dunnev941@gmail.com>
2024-02-26 08:51:33 -0500

    Second Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ Console.WindowHeight = 40; // Sets the window height to 40 lines
 // Visible program outputs and inputs
 Console.Write("Enter your name: "); // Prints the text in quotations to the console
 string userInput = Console.ReadLine(); // Input line for user input that is saved to a string variable after hitting enter
-Console.WriteLine("Thank you " + userInput); // Creates a new line of text that includes what the user typed
+Console.WriteLine("Thank you " + userInput +"\n"); // Creates a new line of text that includes what the user typed
+Console.Write("Please input two numbers that you would like to multiply\n Number 1: "); 
+int number1 = Convert.ToInt32(Console.ReadLine()); // Converts user string input into an integer and saves as int variable
+Console.Write(" Number 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number1 + " multiplied by " + number2 + " = " + number1*number2);
+
 
 // Exit the program
 Console.ReadKey(); // Checks for a key press and closes the program on press
